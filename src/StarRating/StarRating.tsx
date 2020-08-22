c5e48c6f5106e7b664e00c30fdd7c34a0b64535a
BitYoungjae <bityoungjae@gmail.com>
2020-08-22 22:06:04 +0900

    refactor: 리팩터링

--- src/StarRating/StarRating.tsx
+++ src/StarRating/StarRating.tsx
@@ -10,104 +10,100 @@ interface StarRatingProps
   maximumStars?: number;
 }
 
-const StarRating: React.FC<StarRatingProps> = ({
-  now,
-  max,
-  sStarSize = '2rem',
-  sStarColor = 'red',
-  sBackgroundColor = '#ddd',
-  maximumStars = 5,
-}) => {
-  const perStar = max / maximumStars;
-  const filledStars = Math.floor(now / perStar);
-  const blankStars = maximumStars - filledStars - 1;
-  const remain = ((now % perStar) / perStar) * 100;
+type ICalculatedStarList = {
+  rate: number;
+  sStarColor: string | undefined;
+  sBackgroundColor: string | undefined;
+}[];
 
-  if (
-    isInInvalidNumber([
+const StarRating: React.FC<StarRatingProps> = React.forwardRef<
+  HTMLDivElement,
+  StarRatingProps
+>(
+  (
+    {
       now,
       max,
-      maximumStars,
-      perStar,
-      filledStars,
-      blankStars,
-      remain,
-    ])
-  ) {
-    console.warn(`잘 못된 값이 입력되고 있습니다.`);
-    console.warn({ now, max, maximumStars });
-
-    return (
-      <StarBox>
-        {Array(maximumStars)
-          .fill(null)
-          .map((_, index) => (
-            <Star
-              key={index}
-              rate={0}
-              sStarColor={sStarColor}
-              sBackgroundColor={sBackgroundColor}
-              sStarSize={sStarSize}
-            />
-          ))}
-      </StarBox>
-    );
-  }
-
-  if (now > max || blankStars < 0) {
-    return (
-      <StarBox>
-        {Array(maximumStars)
-          .fill(null)
-          .map((_, index) => (
-            <Star
-              key={index}
-              rate={100}
-              sStarColor={sStarColor}
-              sBackgroundColor={sBackgroundColor}
-              sStarSize={sStarSize}
-            />
-          ))}
-      </StarBox>
-    );
-  }
-
-  const calculatedStarList = [
-    ...getStarList({
-      count: filledStars,
-      rate: 100,
-      sStarColor,
-      sBackgroundColor,
-    }),
-    {
-      rate: remain,
-      sStarColor,
-      sBackgroundColor,
+      sStarSize = '2rem',
+      sStarColor = 'red',
+      sBackgroundColor = '#ddd',
+      maximumStars = 5,
     },
-    ...getStarList({
-      count: blankStars,
-      rate: 0,
-      sStarColor,
-      sBackgroundColor,
-    }),
-  ];
+    ref
+  ) => {
+    const perStar = max / maximumStars;
+    const filledStars = Math.floor(now / perStar);
+    const blankStars = maximumStars - filledStars - 1;
+    const remain = ((now % perStar) / perStar) * 100;
+    let calculatedStarList: ICalculatedStarList = [];
 
-  return (
-    <StarBox>
-      {calculatedStarList.map(
-        ({ rate, sBackgroundColor, sStarColor }, index) => (
-          <Star
-            key={index}
-            rate={rate}
-            sStarColor={sStarColor}
-            sBackgroundColor={sBackgroundColor}
-            sStarSize={sStarSize}
-          />
-        )
-      )}
-    </StarBox>
-  );
-};
+    if (
+      isInInvalidNumber([
+        now,
+        max,
+        maximumStars,
+        perStar,
+        filledStars,
+        blankStars,
+        remain,
+      ])
+    ) {
+      console.warn(`잘 못된 값이 입력되고 있습니다.`);
+      console.warn({ now, max, maximumStars });
+
+      calculatedStarList = getStarList({
+        rate: 0,
+        count: maximumStars,
+        sStarColor,
+        sBackgroundColor,
+      });
+    } else if (now > max || blankStars < 0) {
+      calculatedStarList = getStarList({
+        rate: 100,
+        count: maximumStars,
+        sStarColor,
+        sBackgroundColor,
+      });
+    } else {
+      calculatedStarList = [
+        ...getStarList({
+          count: filledStars,
+          rate: 100,
+          sStarColor,
+          sBackgroundColor,
+        }),
+        {
+          rate: remain,
+          sStarColor,
+          sBackgroundColor,
+        },
+        ...getStarList({
+          count: blankStars,
+          rate: 0,
+          sStarColor,
+          sBackgroundColor,
+        }),
+      ];
+    }
+
+    return (
+      <StarBox>
+        {calculatedStarList.map(
+          ({ rate, sBackgroundColor, sStarColor }, index) => (
+            <Star
+              key={index}
+              ref={ref}
+              rate={rate}
+              sStarColor={sStarColor}
+              sBackgroundColor={sBackgroundColor}
+              sStarSize={sStarSize}
+            />
+          )
+        )}
+      </StarBox>
+    );
+  }
+);
 
 const isInInvalidNumber = (list: number[]) => {
   return list.some((v) => Number.isNaN(v) || !Number.isFinite(v));
