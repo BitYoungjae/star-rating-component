0b16a2b093a93f1d6dff08fcf5c77ed1fc55b040
BitYoungjae <bityoungjae@gmail.com>
2020-08-22 22:11:10 +0900

    refactor: 타입관련 리팩터링

--- src/StarRating/StarRating.tsx
+++ src/StarRating/StarRating.tsx
@@ -16,96 +16,94 @@ type ICalculatedStarList = {
   sBackgroundColor: string | undefined;
 }[];
 
-const StarRating: React.FC<StarRatingProps> = React.forwardRef<
+const StarRating: React.ForwardRefRenderFunction<
   HTMLDivElement,
   StarRatingProps
->(
-  (
-    {
+> = (
+  {
+    now,
+    max,
+    sStarSize = '2rem',
+    sStarColor = 'red',
+    sBackgroundColor = '#ddd',
+    maximumStars = 5,
+  },
+  ref
+) => {
+  const perStar = max / maximumStars;
+  const filledStars = Math.floor(now / perStar);
+  const blankStars = maximumStars - filledStars - 1;
+  const remain = ((now % perStar) / perStar) * 100;
+  let calculatedStarList: ICalculatedStarList = [];
+
+  if (
+    isInInvalidNumber([
       now,
       max,
-      sStarSize = '2rem',
-      sStarColor = 'red',
-      sBackgroundColor = '#ddd',
-      maximumStars = 5,
-    },
-    ref
-  ) => {
-    const perStar = max / maximumStars;
-    const filledStars = Math.floor(now / perStar);
-    const blankStars = maximumStars - filledStars - 1;
-    const remain = ((now % perStar) / perStar) * 100;
-    let calculatedStarList: ICalculatedStarList = [];
+      maximumStars,
+      perStar,
+      filledStars,
+      blankStars,
+      remain,
+    ])
+  ) {
+    console.warn(`잘 못된 값이 입력되고 있습니다.`);
+    console.warn({ now, max, maximumStars });
 
-    if (
-      isInInvalidNumber([
-        now,
-        max,
-        maximumStars,
-        perStar,
-        filledStars,
-        blankStars,
-        remain,
-      ])
-    ) {
-      console.warn(`잘 못된 값이 입력되고 있습니다.`);
-      console.warn({ now, max, maximumStars });
-
-      calculatedStarList = getStarList({
-        rate: 0,
-        count: maximumStars,
-        sStarColor,
-        sBackgroundColor,
-      });
-    } else if (now > max || blankStars < 0) {
-      calculatedStarList = getStarList({
+    calculatedStarList = getStarList({
+      rate: 0,
+      count: maximumStars,
+      sStarColor,
+      sBackgroundColor,
+    });
+  } else if (now > max || blankStars < 0) {
+    calculatedStarList = getStarList({
+      rate: 100,
+      count: maximumStars,
+      sStarColor,
+      sBackgroundColor,
+    });
+  } else {
+    calculatedStarList = [
+      ...getStarList({
+        count: filledStars,
         rate: 100,
-        count: maximumStars,
         sStarColor,
         sBackgroundColor,
-      });
-    } else {
-      calculatedStarList = [
-        ...getStarList({
-          count: filledStars,
-          rate: 100,
-          sStarColor,
-          sBackgroundColor,
-        }),
-        {
-          rate: remain,
-          sStarColor,
-          sBackgroundColor,
-        },
-        ...getStarList({
-          count: blankStars,
-          rate: 0,
-          sStarColor,
-          sBackgroundColor,
-        }),
-      ];
-    }
-
-    return (
-      <StarBox ref={ref}>
-        {calculatedStarList.map(
-          ({ rate, sBackgroundColor, sStarColor }, index) => (
-            <Star
-              key={index}
-              rate={rate}
-              sStarColor={sStarColor}
-              sBackgroundColor={sBackgroundColor}
-              sStarSize={sStarSize}
-            />
-          )
-        )}
-      </StarBox>
-    );
+      }),
+      {
+        rate: remain,
+        sStarColor,
+        sBackgroundColor,
+      },
+      ...getStarList({
+        count: blankStars,
+        rate: 0,
+        sStarColor,
+        sBackgroundColor,
+      }),
+    ];
   }
-);
+
+  return (
+    <StarBox ref={ref}>
+      {calculatedStarList.map(
+        ({ rate, sBackgroundColor, sStarColor }, index) => (
+          <Star
+            key={index}
+            rate={rate}
+            sStarColor={sStarColor}
+            sBackgroundColor={sBackgroundColor}
+            sStarSize={sStarSize}
+          />
+        )
+      )}
+    </StarBox>
+  );
+};
 
 const isInInvalidNumber = (list: number[]) => {
   return list.some((v) => Number.isNaN(v) || !Number.isFinite(v));
 };
 
-export default StarRating;
+export default React.forwardRef(StarRating);
